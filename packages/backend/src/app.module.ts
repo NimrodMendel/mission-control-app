ad25fd7f6cff43a8a2075c510dfc2f4909c001e2
NimrodMendel <mendel.nimrod@gmail.com>
2023-09-25 22:35:30 +0300

    refactor to nestjs

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Config } from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: Config.db_host,
+      port: Config.db_port,
+      username: Config.db_user,
+      password: Config.db_password,
+      database: Config.db_database,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
