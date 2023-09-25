22497cf99b82b0939778eb2662229c9091278b24
NimrodMendel <mendel.nimrod@gmail.com>
2023-09-25 17:54:21 +0300

    Add frontend package

---
+++ packages/dashboard/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button color="blue">Click !</Button>
+    </>
+  );
+}
+
+export default App;
